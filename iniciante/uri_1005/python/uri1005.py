cf79dee73560501aace747b8eaa8f94bc9d0c5cc
marlloncsoares <marlonmcs@hotmail.com>
2021-09-06 11:11:48 -0300

    Troubleshooting URI 1005 using Python.

---
+++ iniciante/uri_1005/python/uri1005.py
@@ -0,0 +1,9 @@
+punctuationA = float(input())
+punctuationB = float(input())
+
+weightA = 3.5
+weightB = 7.5
+
+average = ((punctuationA * weightA) + (punctuationB * weightB)) / (weightA + weightB)
+
+print("MEDIA = %.5f" %average)
