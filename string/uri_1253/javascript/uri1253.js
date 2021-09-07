702ddccdf67dae322f822ead9618c336ba222613
marlloncsoares <marlonmcs@hotmail.com>
2021-09-07 01:12:52 -0300

    Start of troubleshooting URI 1253 using JavaScript

---
+++ string/uri_1253/javascript/uri1253.js
@@ -0,0 +1,16 @@
+const posChar, posChave, testes, i, j, c, ch, chave;
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const text;
+
+let input = require('fs').readFileSync('stdin', 'utf8');
+let values = input.split('\n');
+
+const [tests] = values;
+tests = parseInt(tests);
+
+for (c = 0; c< tests; c++) {
+    input = require('fs').readFileSync('stdin', 'utf8');
+    for (i = 0; i < text.length; i++) {}
+}
+
+// TO-DO: Terminar algoritmo
