bb0e83093aecf4625f43c8d8aa89283b21d381bf
marlloncsoares <marlonmcs@hotmail.com>
2021-09-08 18:30:13 -0300

    Troubleshooting URI-1012 using JavaScript.

---
+++ iniciante/uri_1012/javascript/uri1012.js
@@ -0,0 +1,28 @@
+const input = require('fs').readFileSync('stdin', 'utf8');
+const values = input.split(' ');
+
+let [valueA, valueB, valueC] = values;
+
+valueA = parseFloat(valueA);
+valueB = parseFloat(valueB);
+valueC = parseFloat(valueC);
+
+const pi = 3.14159;
+const triangleArea = (valueA * valueC / 2);
+const circleArea = (pi * Math.pow(valueC, 2));
+const trapezeArea = ((valueA + valueB) * valueC) / 2;
+const squareArea = (valueB * valueB);
+const rectangleArea = (valueA * valueB);
+
+const decimalPlaces = 3;
+const triangleAreaView = Number(triangleArea).toFixed(decimalPlaces);
+const circleAreaView = Number(circleArea).toFixed(decimalPlaces);
+const trapezeAreaView = Number(trapezeArea).toFixed(decimalPlaces);
+const squareAreaView = Number(squareArea).toFixed(decimalPlaces);
+const rectangleAreaView = Number(rectangleArea).toFixed(decimalPlaces);
+
+console.log('TRIANGULO: ' + triangleAreaView);
+console.log('CIRCULO: ' + circleAreaView);
+console.log('TRAPEZIO: ' + trapezeAreaView);
+console.log('QUADRADO: ' + squareAreaView);
+console.log('RETANGULO: ' + rectangleAreaView);
